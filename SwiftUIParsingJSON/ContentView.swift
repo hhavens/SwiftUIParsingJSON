7a88960ec147c2a434d591e3e9c0b4ce07d2240d
Henry Havens <hhavens@hotmail.com>
2020-08-04 08:38:28 -0500

    Initial Commit

---
+++ SwiftUIParsingJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIParsingJSON
+//
+//  Created by Henry Havens on 8/4/20.
+//  Copyright © 2020 Henry Havens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
